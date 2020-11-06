9e6034eeae5034f809466e707fbf4d9cff54900c
Larry Edelstein <ledelstein@perforce.com>
2020-11-06 22:56:34 +0000

    remove nonsense

--- base_analysis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-kwinject gcc foo.c -o nothing
-kwbuildproject --url http://localhost:8190/projectile -o kwtables kwinject.out
-kwadmin load --url http://localhost:8190 projectile kwtables
